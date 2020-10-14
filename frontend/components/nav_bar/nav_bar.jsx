64105ac3a4cbe15679c18d7588517431e8b4056e
Zach Vaillancourt <zsvail98@live.com>
2020-10-13 17:30:26 -0700

    test

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 
 export default () => (    
     <div className="global-nav">
-        
-        <div className="global-logo">
-            <div className="specific-logo-container">
-                <img className="logo" src="https://ksr-static.imgix.net/tq0sfld-kickstarter-logo-green.png?ixlib=rb-2.1.0&s=0cce952d7b55823ff451a58887a0c578"/>
-            </div>
-        </div>
 
         <div className="left-menu">
             <a href="#">Explore</a>
             <a href="#">Start a project</a>
+        </div>
+
+        <div className="global-logo">
+            <div className="specific-logo-container">
+                <img className="logo" src="https://ksr-static.imgix.net/tq0sfld-kickstarter-logo-green.png?ixlib=rb-2.1.0&s=0cce952d7b55823ff451a58887a0c578"/>
+            </div>
         </div>
 
         <div className="right-menu">
