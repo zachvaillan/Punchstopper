97cc3f3315e7070bf86407e3087da7e6095e5cb2
Zach <zachsvaillancourt@gmail.com>
2020-12-16 14:37:54 -0700

    add tables for rewards and backings

---
+++ db/migrate/20201216211344_create_rewards.rb
@@ -0,0 +1,12 @@
+class CreateRewards < ActiveRecord::Migration[5.2]
+  def change
+    create_table :rewards do |t|
+      t.integer :min_amount, null: false
+      t.string :title, null: false
+      t.string :description, null: false
+      t.date :deliver_date, null: false
+      t.integer :backer_count, null: false, :default => 0
+      t.integer :project_id, null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_16_002711) do
+ActiveRecord::Schema.define(version: 2020_12_16_212955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,11 @@ ActiveRecord::Schema.define(version: 2020_12_16_002711) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "backs", force: :cascade do |t|
+    t.integer "backer_id", null: false
+    t.integer "reward_id", null: false
+  end
+
   create_table "projects", force: :cascade do |t|
     t.string "title"
     t.string "category", null: false
@@ -51,6 +56,15 @@ ActiveRecord::Schema.define(version: 2020_12_16_002711) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["owner_id"], name: "index_projects_on_owner_id"
+  end
+
+  create_table "rewards", force: :cascade do |t|
+    t.integer "min_amount", null: false
+    t.string "title", null: false
+    t.string "description", null: false
+    t.date "deliver_date", null: false
+    t.integer "backer_count", default: 0, null: false
+    t.integer "project_id", null: false
   end
 
   create_table "users", force: :cascade do |t|
